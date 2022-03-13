f0dad424cf7479c3b96fad11cdef0aae2d5b40f5
Achraf TAJANI <achraf.tajani-ext@bnf.fr>
2022-03-13 18:33:35 +0100

    add route

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello Worldffff!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
