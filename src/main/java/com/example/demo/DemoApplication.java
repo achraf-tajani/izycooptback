8312ef10312c4ffbb37b8e6db4b5af02d3108892
Achraf TAJANI <achraf.tajani-ext@bnf.fr>
2022-03-13 17:43:05 +0100

    change name projet

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,14 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
 @SpringBootApplication
 public class DemoApplication {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
